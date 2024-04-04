ea76b1eafb60c6544373348eca4fcbd57a308c6c
Rafael <rafavmaximo@gmail.com>
2024-04-04 09:41:57 -0300

    :wave:Começo do Array

--- src/app/cadatros/cadatros.component.ts
+++ src/app/cadatros/cadatros.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadatros',
   templateUrl: './cadatros.component.html',
   styleUrl: './cadatros.component.css'
 })
-export class CadatrosComponent {
+export class CadatrosComponent implements OnInit {
+  
+  products: Product [] = [];
 
+  formsGroupProduct : FormGroup | undefined;
+
+  constructor(private  formBuider : FormBuilder){
+
+    this.formsGroupProduct = formBuider.group({
+    name : [''],
+    id : [''],
+    description : [''],
+    price : [''],
+    quantidade: [''],
+    responsable: ['']
+    })
+  }
+  
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+  
+  loadProduct(){
+
+  }
+    
 }
---
+++ src/app/product.ts
@@ -0,0 +1,8 @@
+export interface Product{
+    name : string,
+    id : number,
+    description : string,
+    price : number,
+    quantidade: number,
+    responsable: string
+}
