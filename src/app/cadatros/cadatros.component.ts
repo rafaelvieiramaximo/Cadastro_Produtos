39178cf07b224d9cd8c6e171188259661d8e432e
Rafael <rafavmaximo@gmail.com>
2024-04-09 11:50:52 -0300

    :exclamation:Validações

--- src/app/cadatros/cadatros.component.ts
+++ src/app/cadatros/cadatros.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../product';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -16,13 +16,15 @@ export class CadatrosComponent implements OnInit {
 
   isEditing: boolean = false;
 
+  isError: boolean = false
+
   constructor(private formBuider: FormBuilder, private service: ProductService) {
 
     this.formGroupProduct = formBuider.group({
-      name: [''],
+      name: ['', [Validators.required, Validators.minLength(4)]],
       id: [''],
-      description: [''],
-      price: [''],
+      description: ['',[Validators.minLength(10), Validators.required] ],
+      price: ['',],
       quantidade: [''],
       responsable: ['']
     })
@@ -38,33 +40,44 @@ export class CadatrosComponent implements OnInit {
     this.loadProduct();
   }
   save() {
-    if (this.isEditing) {
-      this.service.update(this.formGroupProduct.value).subscribe({
-        next: () => {
-          this.loadProduct();
-          this.isEditing = false;
-          this.formGroupProduct.reset();
-        }
+    if (this.formGroupProduct.valid) {
+      if (this.isEditing) {
+        this.service.update(this.formGroupProduct.value).subscribe({
+          next: () => {
+            this.loadProduct();
+            this.isEditing = false;
+            this.formGroupProduct.reset();
+            this.isError = false
+          }
+        })
       }
-      )
-    }
-    else {
-      this.service.save(this.formGroupProduct.value).subscribe({
-        next: data => this.products.push(data)
-      });
-      this.formGroupProduct.reset();
+      else {
+        this.service.save(this.formGroupProduct.value).subscribe({
+          next: data => this.products.push(data)
+        });
+        this.formGroupProduct.reset();
+        this.isError = false
+      }
+    }else{
+      this.isError = true
     }
   }
-
-  delete(product:Product){
+  delete(product: Product) {
     this.service.delete(product).subscribe({
-      next: ()=> this.loadProduct()
+      next: () => this.loadProduct()
     })
   }
 
-  edit(product:Product){
+  edit(product: Product) {
     this.formGroupProduct.setValue(product);
     this.isEditing = true;
   }
 
+  get name(): any {
+    return this.formGroupProduct.get("name")
+  }
+
+  get description(): any {
+    return this.formGroupProduct.get("description")
+  }
 }
