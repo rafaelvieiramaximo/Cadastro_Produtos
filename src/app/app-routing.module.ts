098b61e2cf4bdad433ae4808abec060517c25a83
Rafael <rafavmaximo@gmail.com>
2024-04-18 11:58:19 -0300

    :horse_racing:Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CadatrosComponent } from './cadatros/cadatros.component';
+import { Error404Component } from './error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product', component: CadatrosComponent},
+  {path: 'not-found' , component: Error404Component},
+  {path: '**', redirectTo: '/not-found'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,19 @@ import { FormsModule } from '@angular/forms';
 import { CadatrosComponent } from './cadatros/cadatros.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadatrosComponent
+    CadatrosComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
