21159022990954116cfc0a1b0fbb0b1e8448077e
Rafael <rafavmaximo@gmail.com>
2024-07-18 12:11:27 -0300

    Teste de compilação, exemplo de um produto

--- back end/product-backend/src/main/java/com/product/product_backend/models/Product.java
+++ back end/product-backend/src/main/java/com/product/product_backend/models/Product.java
@@ -1,14 +1,14 @@
 package com.product.product_backend.models;
 
 public class Product {
-    
-    private String name;
+   
     private int id;
+    private String name;
     private String description;
     private double price;
     private Number quantidade;
     private String responsable;
-    
+
     public String getName() {
         return name;
     }
--- back end/product-backend/src/main/java/com/product/product_backend/resourses/ProductController.java
+++ back end/product-backend/src/main/java/com/product/product_backend/resourses/ProductController.java
@@ -3,13 +3,24 @@ package com.product.product_backend.resourses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.product.product_backend.models.Product;
+
 @RestController
 public class ProductController {
     
     @GetMapping ("product")
-    public String getProduct(){
+    public Product getProduct(){
 
-        return ("Sou um produto");
+        Product p = new Product();
+
+        p.setId(1);
+        p.setName("Coca cola");
+        p.setPrice(10.50);
+        p.setQuantidade(1);
+        p.setDescription("Geladinha");
+        p.setResponsable("Marquinhos");
+
+        return p;
     }
 
 }
