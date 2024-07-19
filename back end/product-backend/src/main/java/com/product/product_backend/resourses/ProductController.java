9adab7284625ddf9d551ad5086364f02bc5ae435
Rafael <rafavmaximo@gmail.com>
2024-07-19 12:25:48 -0300

    Estruturando message certa e limpando a tela de erro

--- back end/product-backend/src/main/java/com/product/product_backend/resourses/ProductController.java
+++ back end/product-backend/src/main/java/com/product/product_backend/resourses/ProductController.java
@@ -3,10 +3,12 @@ package com.product.product_backend.resourses;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.product.product_backend.models.Product;
 
@@ -50,7 +52,7 @@ public class ProductController {
             return ResponseEntity.ok(products.get(id-1));
 
         }else{
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Product not Found in BD");
         }
     }
     
